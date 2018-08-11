877c56e2cf7c1d82fa3ed5bcbeaae6ff4a4b8d0a
sennhvi <sennhvi@gmail.com>
2018-08-11 17:59:33 +0800

    add Lv1C2Q5

--- Lv1C2/Lv1C2.cpp
+++ Lv1C2/Lv1C2.cpp
@@ -76,5 +76,29 @@ int Lv1C2Q4() {
 }
 
 int Lv1C2Q5() {
+    // 判断2017年某月天数
+    int m, d;
+    cin >> m;
+    switch (m) {
+        case 2:
+            d = 28;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            d = 30;
+            break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            d = 31;
+            break;
+    }
+    cout << d;
     return 0;
 }
