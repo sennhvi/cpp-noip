dbcbb95dfa9fb96c1667b06866b4d5d42033f046
sennhvi <sennhvi@gmail.com>
2018-08-12 16:58:31 +0800

    update Lv1C3

--- Lv1C3/Lv1C3.cpp
+++ Lv1C3/Lv1C3.cpp
@@ -3,16 +3,15 @@
 */
 #include <iostream>
 #include <iomanip>
+#include <cstdio>
 
 using namespace std;
 
 int Lv1C3Q1() {
     // 输入指定个数字并求和
-    int a;
+    int a, b, sum = 0;  // assignment in declaration
     cin >> a;
     // cin >> b >> c >> d;
-    int b;
-    int sum = 0;
     for (int i = 0; i < a; i++) {
         cin >> b;
         sum += b;
@@ -22,7 +21,16 @@ int Lv1C3Q1() {
 }
 
 int Lv1C3Q2() {
-//
+    // 求n个正整数的平均值，结果保留2位小数
+    // Win: Enter, Ctrl+Z, Enter
+    // Linux: Ctrl+D
+    int a, count = 0;
+    double sum = 0;
+    while (scanf("%d", &a) == 1) {
+        sum += a;
+        count++;
+    }
+    printf("%0.2f", sum / count);
     return 0;
 }
 
--- Lv1C3/main.cpp
+++ Lv1C3/main.cpp
@@ -4,7 +4,7 @@
 #include "Lv1C3.h"
 
 int main() {
-    Lv1C3Q1();
+//    Lv1C3Q1();
     Lv1C3Q2();
     Lv1C3Q3();
     Lv1C3Q4();
