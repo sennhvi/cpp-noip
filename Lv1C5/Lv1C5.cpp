da573565cb60cd451e86faac10a74e0ab3c3b1b9
sennhvi <sennhvi@gmail.com>
2018-08-17 02:11:38 +0800

    update Lv1C5Q1-2

--- Lv1C5/Lv1C5.cpp
+++ Lv1C5/Lv1C5.cpp
@@ -1,29 +1,54 @@
 /*
     Implement functions in this file
 */
-#include <iostream> 
+#include <iostream>
 #include <iomanip>
 
 using namespace std;
 
+
+int size(int length, int width);
+
+bool check(int a, int b);
+
+
 int Lv1C5Q1() {
-	//
+    // 输入两个正整数分别代表矩形的两条边长，两个数字用一个空格隔开，要求输出一个正整数表明矩形的面积。
+    int a, b;
+    cin >> a >> b;
+    cout << size(a, b);
     return 0;
 }
 
 int Lv1C5Q2() {
-	//
+    // 有两个整数a，b，其中0，0<=b<10，当整数a中包括数字b时，输出结果为true，否则输出结果为false。
+    int a, b;
+    cin >> a >> b;
+    if (check(a, b)) cout << "true";
+    else cout << "false";
+
     return 0;
 }
 
 int Lv1C5Q3() {
-	//
+    //
     return 0;
 }
 
 int Lv1C5Q4() {
-	//
+    //
     return 0;
 }
 
 
+int size(int length, int width) {
+    return length * width;
+}
+
+bool check(int a, int b) {
+    while (a) {
+        if (a % 10 == b) return true;
+        else a /= 10;
+    }
+    return false;
+}
--- Lv1C5/main.cpp
+++ Lv1C5/main.cpp
@@ -4,7 +4,7 @@
 #include "Lv1C5.h"
 
 int main() {
-    Lv1C5Q1();
+//    Lv1C5Q1();
     Lv1C5Q2();
     Lv1C5Q3();
     Lv1C5Q4();
