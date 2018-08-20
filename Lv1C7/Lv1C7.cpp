6c1f660d004e77a4d58e9dc4c8e8c3c8d70ce54e
sennhvi <sennhvi@gmail.com>
2018-08-21 00:31:50 +0800

    add Lv1C7Q2

--- Lv1C7/Lv1C7.cpp
+++ Lv1C7/Lv1C7.cpp
@@ -8,6 +8,14 @@
 
 using namespace std;
 
+int transfer(int a[], char s[]) {
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        a[len - i - 1] = s[i] - '0';
+    }
+}
+
+
 int Lv1C7Q1() {
     // 输入一个N位正整数S，其中0。判断该正整数能否被3整除，如果能，输出：Yes，如果不能，输出：No。
     /**
@@ -31,7 +39,25 @@ int Lv1C7Q1() {
 }
 
 int Lv1C7Q2() {
-    //
+    // 在两行中分别输入两个L1位和L2位正整数A1和A2，满足0，一个数字占一行，要求计算两个数的和S，并在一行中输出。
+    char s1[100], s2[100];
+    int a1[100], a2[100], ans[101];
+    int len1, len2, s = 0, c = 0; // s: 计算位，c: 进位
+    memset(ans, 0, sizeof(ans)); // memset 将数组中所有元素都重置为0
+    memset(a1, 0, sizeof(a1));
+    memset(a2, 0, sizeof(a2));
+    scanf("%s\n%s", s1, s2);
+    len1 = transfer(a1, s1);
+    len2 = transfer(a2, s2);
+    while (s <= len1 || s <= len2) {
+        ans[s] = a1[s] + a2[s] + c;
+        c = ans[s] / 10;
+        ans[s] %= 10;
+        s++;
+    }
+    if (ans[s - 1] == 0) s--;
+    for (int i = s - 1; i >= 0; i--) printf("%d", ans[i]);
+
     return 0;
 }
 
